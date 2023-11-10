75b35c07fe3ce22d97f33ce87d1f42f47164fdc9
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-10 16:39:58 +0330

    dark mode theme toggle

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,14 @@
-import styles from './themeToggle.module.css'
+import Image from "next/image";
+import styles from "./themeToggle.module.css";
 
 const ThemeToggle = () => {
   return (
-    <div className={styles.container}>ThemeToggle</div>
-  )
-}
+    <div className={styles.container}>
+      <Image src={"/moon.png"} alt="" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src={"/sun.png"} alt="" width={14} height={14} />
+    </div>
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
