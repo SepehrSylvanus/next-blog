e0de4415b2158846b89cb1f494325d82bad9f68b
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-11 15:59:26 +0330

    side menu component UI

--- src/app/components/menu/Menu.jsx
+++ src/app/components/menu/Menu.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import styles from './menu.module.css'
+import React from "react";
+import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 const Menu = () => {
   return (
-    <div className={styles.container}>Menu</div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What's hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+    <MenuPosts withImage={false}/>
 
-export default Menu
+      {/* CENTER CATEGORIES */}
+      <h2 className={styles.subtitle}>DIscover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+     <MenuCategories/>
+
+      {/* SECOND */}
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors pick</h1>
+      <MenuPosts withImage={true}/>
+    </div>
+  );
+};
+
+export default Menu;
