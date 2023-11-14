41c35df692129109e80c39195b841fdc776bd378
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-14 23:01:25 +0330

    blog app schima

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -2,76 +2,44 @@ import React from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
-const CategoryList = () => {
+
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CategoryList = async () => {
+  const data = await getData();
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        
-          <Link href={'/blog?cat=style'} className={`${styles.category} ${styles.style}`}>
-            <Image
-              src={"/style.png"}
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            style
+        {data.map((item) => (
+          <Link
+            href={"/blog?cat=style"}
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
           </Link>
-        
-          <Link href={'/blog?cat=style'} className={`${styles.category} ${styles.fashion}`}>
-            <Image
-              src={"/fashion.png"}
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            fashion
-          </Link>
-        
-          <Link href={'/blog?cat=style'} className={`${styles.category} ${styles.food}`}>
-            <Image
-              src={"/food.png"}
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            food
-          </Link>
-        
-          <Link href={'/blog?cat=style'} className={`${styles.category} ${styles.travel}`}>
-            <Image
-              src={"/travel.png"}
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            travel
-          </Link>
-          <Link href={'/blog?cat=style'} className={`${styles.category} ${styles.culture}`}>
-            <Image
-              src={"/culture.png"}
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            culture
-          </Link>
-          <Link href={'/blog?cat=style'} className={`${styles.category} ${styles.coding}`}>
-            <Image
-              src={"/coding.png"}
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            coding
-          </Link>
-       
+        ))}
       </div>
     </div>
   );
