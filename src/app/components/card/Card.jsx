8be61446bce01cea286bd7c329211fdfece8d271
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-15 12:42:05 +0330

    send query

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,29 +1,28 @@
 import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
-const Card = ({key, item}) => {
+const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src={"/p1.jpeg"} alt="" fill className={styles.image} />
-      </div>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href={"/"}>
-          <h1>
-            {item.title}
-          </h1>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam,
-          adipisci ducimus quia mollitia debitis architecto inventore
-          consequuntur assumenda iusto placeat nihil earum id natus sequi
-          dignissimos magnam corporis, vitae obcaecati!
-        </p>
-        <Link href={"/"} className={styles.link}>Read More</Link>
+        <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
