f812f87d5c53f6a80915a43241ea61d657d1f777
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2023-11-15 15:46:19 +0330

    post a comment

--- src/app/components/comments/Comments.jsx
+++ src/app/components/comments/Comments.jsx
@@ -4,6 +4,7 @@ import styles from "./comments.module.css";
 import Image from "next/image";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -19,11 +20,23 @@ const fetcher = async (url) => {
 };
 
 const Comments = ({ postSlug }) => {
-  const {status} = useSession();
+  const { status } = useSession();
 
-  const { data, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher
+  const { data, mutate, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
   );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+
+    mutate()
+  };
 
   return (
     <div className={styles.container}>
@@ -31,34 +44,41 @@ const Comments = ({ postSlug }) => {
 
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder="write a comment..." className={styles.input} />
-          <button className={styles.button}>Send</button>
+          <textarea
+            placeholder="write a comment..."
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
         <Link href={"/"}>Login to write a comment</Link>
       )}
       <div className={styles.comments}>
-      {isLoading ? 'Loading...' : data.map(item => (
-          <div className={styles.comment} key={item._id}>
-          <div className={styles.user}>
-            { item?.user?.image && <Image
-              src={item.user.image}
-              alt=""
-              width={50}
-              height={50}
-              className={styles.image}
-            />}
-            <div className={styles.userInfo}>
-              <span className={styles.username}>{item.user.name}</span>
-              <span className={styles.date}>{item.createdAt}</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-          {item.desc}
-          </p>
-        </div>
-    
-      ))}
+        {isLoading
+          ? "Loading..."
+          : data.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item.desc}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
